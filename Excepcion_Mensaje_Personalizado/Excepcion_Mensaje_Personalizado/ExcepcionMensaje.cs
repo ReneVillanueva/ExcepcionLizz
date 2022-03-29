914be36b68c22398f62f008e502e845ec0478f89
ReneVillanueva <88916739+ReneVillanueva@users.noreply.github.com>
2022-03-29 15:15:08 -0700

    Add files via upload

---
+++ Excepcion_Mensaje_Personalizado/Excepcion_Mensaje_Personalizado/ExcepcionMensaje.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepcion_Mensaje_Personalizado
+{
+    internal class ExcepcionMensaje : Exception
+    {
+        public ExcepcionMensaje(string MensajePersonalizado) 
+        {
+            Console.WriteLine(MensajePersonalizado);
+        }
+    }
+}
